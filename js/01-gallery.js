b20b7b35c0660285e16bbb379a1c3ac67d30b55c
Kardiologpythona <91539787+Kardiologpythona@users.noreply.github.com>
2024-03-30 13:16:15 +0100

    Update gallery01.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,57 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const head = document.querySelector("head");
+
+const link = document.createElement("link");
+
+link.href = src="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js";
+
+head.append(link);
+
+const biblioteka = document.createElement("script");
+
+biblioteka.src =src="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js";
+
+gallery.after(biblioteka);
+
+
+  function createImages(){
+    for(let i = 0; i < galleryItems.length; i++){
+        const divTag = document.createElement("div");
+        
+        const linkTag = document.createElement("a");
+
+         const imageTag = document.createElement("img");
+        
+        divTag.classList.add("gallery_item");
+
+        linkTag.classList.add("gallery__link");
+        //linkTag.href = galleryItems[i].original;
+        
+        imageTag.classList.add("gallery__image");
+        imageTag.src = galleryItems[i].preview;
+        imageTag.setAttribute("data-source", galleryItems[i].original);
+        imageTag.alt = galleryItems[i].description;
+        
+        gallery.append(divTag);
+        divTag.append(linkTag);
+        linkTag.append(imageTag);
+  }
+  
+}
+createImages();
+
+function selectImage(event){
+
+  const selectedImage = event.target.dataset.source;
+  const lightbox = basicLightbox.create(`<img src="${selectedImage}">`);
+      lightbox.show();
+
+}
+
+gallery.addEventListener("click", selectImage);
+
